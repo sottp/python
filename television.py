1f13fa165da66b7af442f0e97838df2cdd565c10
sottp <151191354+sottp@users.noreply.github.com>
2023-11-17 11:32:19 -0600

    power, mute, and channels work

--- television.py
+++ television.py
@@ -9,16 +9,42 @@ class Television:
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
+        self.__premute = self.__volume
         pass
+    pass
 
     def power(self):
+        if self.__status:
+            self.__status = False
+            pass
+        else:
+            self.__status = True
+            pass
         pass
+    pass
 
     def mute(self):
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+                pass
+            else:
+                self.__muted = True
+            pass
         pass
+    pass
 
     def channel_up(self):
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+                pass
+            else:
+                self.__channel = Television.MIN_CHANNEL
+                pass
+            pass
         pass
+    pass
 
     def channel_down(self):
         if self.__status:
@@ -30,6 +56,7 @@ class Television:
                 pass
             pass
         pass
+    pass
 
     def volume_up(self):
         if self.__status:
@@ -45,8 +72,8 @@ class Television:
 
     def __str__(self):
         if self.__muted:
-            return f'Volume = {Television.MIN_VOLUME}'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         else:
-            return f'xxx'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
         pass
     pass

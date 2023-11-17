68b9e75b58eaf29277938a89f501e054ef2534df
sottp <151191354+sottp@users.noreply.github.com>
2023-11-17 12:57:02 -0600

    testing messages

--- television.py
+++ television.py
@@ -13,6 +13,9 @@ class Television:
         pass
 
     def power(self):
+        """
+        used to turn off and on the tv
+        """
         if self.__status:
             self.__status = False
             pass
@@ -22,6 +25,9 @@ class Television:
         pass
 
     def mute(self):
+        """
+        used to mute and unmute the tv while it is powered on
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -32,6 +38,9 @@ class Television:
         pass
 
     def channel_up(self):
+        """
+        used to increase the tv channel value while the tv is powered on
+        """
         if self.__status:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
@@ -43,6 +52,9 @@ class Television:
         pass
 
     def channel_down(self):
+        """
+        used to decrease the tv channel value while the tv is powered on
+        """
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -54,6 +66,9 @@ class Television:
         pass
 
     def volume_up(self):
+        """
+        used to increase the tv volume while the tv is powered on
+        """
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
@@ -63,6 +78,9 @@ class Television:
         pass
 
     def volume_down(self):
+        """
+        used to decrease the tv volume while it is powered on
+        """
         if self.__status:
             self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
@@ -72,6 +90,9 @@ class Television:
         pass
 
     def __str__(self):
+        """
+        used to show the tv status
+        """
         if self.__muted:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         else:
